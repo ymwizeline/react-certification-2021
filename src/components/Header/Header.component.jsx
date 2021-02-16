1827eeb985021fd3a19b988a1a95356e7e215208
Yvan Menuet <yvan.menuet@wizeline.com>
2021-02-16 17:13:00 -0600

    install styled-components package, create Header component and import it in Layout component. also changed src/index.js to src/index.jsx because on eslint.

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.nav`
+  height: 80px;
+  width: 100%;
+`;
+
+const Header = () => {
+  return <StyledHeader>Hello</StyledHeader>;
+};
+
+export default Header;
--- src/components/Layout/Layout.component.jsx
+++ src/components/Layout/Layout.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Header from '../Header';
 import './Layout.styles.css';
 
 function Layout({ children }) {
-  return <main className="container">{children}</main>;
+  return (
+    <main className="container">
+      <Header />
+      {children}
+    </main>
+  );
 }
 
 export default Layout;
